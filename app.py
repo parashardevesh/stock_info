ef6e14154c7262062d4e1fc770a52612aafe197d
Devesh Parashar <deveshdp31oct@gmail.com>
2024-01-27 12:41:32 +0530

    added form and 2 class models
    initiated a one to many relationship between User class and Transation class. just testing how it works

--- app.py
+++ app.py
@@ -1,14 +1,79 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, url_for, request
 import yfinance as yf
 import pandas as pd
 import json
-
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '01c59a8611d6bc670d64ae69b2a2df'
+app.config['SQL_DATABASE_URI']='sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(50), nullable = False)
+    transactions = db.relationship('Transaction', backref='portfolio', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(10), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False, server_default=db.func.current.data())
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Transaction('{self.stock_symbol}','{self.quantity}','{self.price}','{self.date}')"
+    
+    
+stock = [
+    {
+        'Name':'IRFC',
+        'Analysts':'Good Buy',
+        'Hold':'One Year',
+        'Target':'250'
+    },
+    {
+        'Name':'IREDA',
+        'Analysts':'Strong Buy',
+        'Hold':'2 years',
+        'Target':'350'
+    }
+]
 
 @app.route('/')
 def Welcome():
     return render_template('index.html')
 
+@app.route('/register', methods = ['GET', 'POST']  )
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', topic = 'Register', form=form)
+
+@app.route('/login', methods = ['GET', 'POST'] )
+def login():
+    form = LoginForm()
+    return render_template('login.html', topic = 'login', form=form)
+
+@app.route('/test/login', methods = ["POST"])
+def test_dummy_login():
+    username = request.json['username']
+    password = request.json['password']
+    
+    if username == 'devesh' and password == 'devesh':
+        return { 'username': username, 'password':password }
+    else:
+        return { 'error': 'user not found' }
 
 @app.route('/<stock_name>')
 def get_stock_info(stock_name):
