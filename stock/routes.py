fd2e2152fe28756b350f7c179dd383d8cea6abfa
Devesh Parashar <deveshdp31oct@gmail.com>
2024-01-28 17:38:23 +0530

    added: profile_picture upload feature
    user can change username and email id and upload profile picture now

--- stock/routes.py
+++ stock/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, flash, url_for, redirect, request
 from stock import app, db, bcrypt
-from stock.forms import RegistrationForm, LoginForm
+from stock.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from stock.models import User, Portfolio
 from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
+import secrets
+import os
 import email_validator
 import yfinance as yf
 import json
@@ -62,11 +65,36 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your Account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email
     image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 @app.route('/test/register', methods = ["POST"])
 def test_dummy_login():
